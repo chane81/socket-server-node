2fe117b105be972f68483c440b7c64e39e080909
BM <chane81@naver.com>
2019-05-04 00:52:01 +0900

    배포환경 로직 변경

--- config/env.js
+++ config/env.js
@@ -2,17 +2,24 @@
  * 사용자 config 세팅 여기서 함(.env 에서 설정하던 방식에서 변경)
  */
 
+// function getParse(val) {
+//   return Object.keys(val).reduce((env, key) => {
+//     env[key] = JSON.stringify(val[key]);
+//     return env;
+//   }, {});
+// }
+
 function getParse(val) {
-  return Object.keys(val).reduce((env, key) => {
-    env[key] = JSON.stringify(val[key]);
-    return env;
-  }, {});
+  return Object.keys(val).reduce((env, key) => 
+    Object.assign({}, env, {
+      [`process.env.${key}`]: val[key]
+    }), {});
 }
 
 function getClientConfig() {
   const target = process.env.npm_lifecycle_event;
-  process.env.NODE_ENV = target === 'build' ? 'production' : 'development';
-
+  process.env.NODE_ENV = target === 'start' ? 'production' : 'development';
+//console.log('fdfd:', target);
 	const envVal = {
     // 개발환경 변수
 		development: {
@@ -21,8 +28,7 @@ function getClientConfig() {
       JWT_PRIVATE_KEY: 'cloud99'
     },
     // 실서버환경 변수
-    production: {
-      SOCKET_IO_PORT: process.env.port,
+    production: {      
       JWT_PRIVATE_KEY: 'black2284'
     }
 	};
@@ -32,16 +38,10 @@ function getClientConfig() {
   const raw = envVal[process.env.NODE_ENV];
 
   // env 에 사용자 config 변수값 삽입
-  // const stringified = {
-  //   'process.env': {
-  //     ...getParse(process.env),
-  //     ...getParse(raw)
-  //   }
-  // };
-
-  const stringified = {
-    'process.env': `Object.assign(${JSON.stringify(raw)}, process.env)`
-  };
+  const stringified = getParse(raw);
+    //'process.env': Object.assign(getParse(raw), getParse(process.env))
+    
+  
 
   console.log('raw:', stringified);
   return { raw, stringified };
