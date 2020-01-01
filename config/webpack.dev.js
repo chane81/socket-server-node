4bd214d092311103c5d0ccde53d5d920ba79618b
BM <chane81@naver.com>
2020-01-01 22:49:16 +0900

    소스수정시 자동 새로고침 적용

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,10 +1,19 @@
 const merge = require('webpack-merge');
 const config = require('./webpack.config');
+const nodemonPlugin = require('nodemon-webpack-plugin');
+const path = require('path');
+
+const rootPath = path.resolve(__dirname, '../');
 
 module.exports = merge(config, {
   mode: 'development',
-  devtool: 'cheap-eval-sourcemap',
-  devServer: {
-    port: 3030,
-  },
+  devtool: 'inline-source-map',
+  plugins: [
+    ...config.plugins,
+    new nodemonPlugin({
+      //nodeArgs: ['-r', 'dotenv/config', '--inspect=0.0.0.0:9229'],
+      watch: path.resolve(rootPath, 'dist'),
+      script: path.resolve(rootPath, 'dist/server.js')
+    })
+  ]
 });
